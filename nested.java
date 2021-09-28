78dcad0f3bcc3e6181fad2681c011847fdd0e80b
9921014491 <91535182+9921014491@users.noreply.github.com>
2021-09-28 16:47:09 +0530

    Uploaded first file

---
+++ nested.java
@@ -0,0 +1,27 @@
+package classwork;
+
+public class nested 
+{
+
+	public static void main(String[] args) 
+	{
+	
+	char n='A';
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=5;i++)
+		{
+		
+			n='A'; 
+			for ( int j=5;j>=i;j--)
+			{
+				System.out.print("n");
+				n++;
+			
+				
+			System.out.println();
+			}
+		}
+	}
+}
+	
